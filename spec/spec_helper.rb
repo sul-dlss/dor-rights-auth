efaae4499c8e41e5d486880fd7bb8f9a00a06dba
Naomi Dushay <ndushay@stanford.edu>
2016-06-06 14:35:59 -0700

    send coverage data to both codeclimate and coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,16 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'bundler/setup'
 require 'rspec'
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 require 'coveralls'
 Coveralls.wear!
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  CodeClimate::TestReporter::Formatter,
+  Coveralls::SimpleCov::Formatter
+]
 
 require 'dor/rights_auth'
