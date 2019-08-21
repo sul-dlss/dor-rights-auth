5599abd5e2e23e5a35157eb3d21edc1429d21722
Johnathan Martin <john.martin@stanford.edu>
2019-08-21 00:37:28 -0700

    add tests for 'dark?' method

--- spec/dor_services_examples_spec.rb
+++ spec/dor_services_examples_spec.rb
@@ -31,5 +31,11 @@ describe Dor::RightsAuth do
     expect(r).to be_stanford_only_unrestricted
     expect(r).not_to be_public_unrestricted
     expect(r.index_elements).to include :primary => 'access_restricted'
+    expect(r.dark?).to be false
+  end
+
+  it 'raises a helpful error when dark? is called, if the object was not instantiated appropriately' do
+    r = Dor::RightsAuth.parse(xml)
+    expect { r.dark? }.to raise_error(RuntimeError, /instantiate by calling '.parse\(xml, forindex = true\)'/)
   end
 end
